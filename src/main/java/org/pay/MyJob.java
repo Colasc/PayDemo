4f3e4590f4d8bb4032677c8fd5f77f163f480b85
xuwenpeng <x1245042275@163.com>
2023-05-30 22:38:55 +0800

    第一种方法：数据库轮询

---
+++ src/main/java/org/pay/MyJob.java
@@ -0,0 +1,27 @@
+package org.pay;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+public class MyJob implements Job {
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println("扫库。。。");
+    }
+
+    public static void main(String[] args) throws Exception {
+        //创建定时任务
+        JobDetail jobDetail = JobBuilder.newJob(Job.class).withIdentity("job1","group1").build();
+
+        //创建触发器，每3秒钟执行一次
+        Trigger trigger = TriggerBuilder.newTrigger().withIdentity("trigger1","group3")
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(3).repeatForever()).build();
+
+        Scheduler scheduler = new StdSchedulerFactory().getScheduler();
+
+        //将任务及其触发器放置在调度器
+        scheduler.scheduleJob(jobDetail,trigger);
+
+        scheduler.start();
+
+    }
+}
